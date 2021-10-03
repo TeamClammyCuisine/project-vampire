917dd57bcb2cc622a0620d4081b19d0ec83c9323
John Sousa <douglas.sousa@gmail.com>
2021-10-04 07:22:14 +0900

    Fixing git ignore Removed old git ignore and copied from lara's project new git ignore.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var test = gameObject;
     }
 
     // Update is called once per frame
